e0bd5b7cb9d8c24631985364f397eb12b3d50d8c
Jose Roberto Torres Mancilla <jroberto@bamboo.local>
2019-09-23 19:45:00 -0500

    Add Deep-first and Breath-first (iterative & recursive) traverse methods.

--- src/com/jroberto/cic/Graph.java
+++ src/com/jroberto/cic/Graph.java
@@ -3,12 +3,16 @@ package com.jroberto.cic;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Graph<T> {
     private HashMap<Vertex<T>, ArrayList<Vertex<T>>> adjacencyMap;
+
+    private Graph<T> dfTree;
+
+    private Graph<T> dfrTree;
+
+    private Graph<T> bfTree;
 
     public Graph() {
         this.adjacencyMap = new HashMap<>();
@@ -18,8 +22,33 @@ public class Graph<T> {
         return this.adjacencyMap;
     }
 
+    public Graph<T> getDfTree() {
+        if (this.dfTree == null) {
+            this.dfTree =  this.deepFirstTraverse();
+        }
+        return dfTree;
+    }
+
+    public Graph<T> getDfrTree() {
+        if (this.dfrTree == null) {
+            this.dfrTree = this.deepFirstRecursiveTraverse();
+        }
+        return this.dfrTree;
+    }
+    
+    public Graph<T> getBfTree() {
+        if (this.bfTree == null) {
+            this.bfTree =  this.breadthFirstTraverse();
+        }
+        return bfTree;
+    }
+    
     public void addVertex(Vertex<T> newVertex) {
         this.adjacencyMap.putIfAbsent(newVertex, new ArrayList<>());
+    }
+
+    public boolean vertexExists(Vertex<T> vertex) {
+        return this.adjacencyMap.containsKey(vertex);
     }
 
     public Vertex<T> getVertex(String vertexLabel) {
@@ -47,6 +76,92 @@ public class Graph<T> {
 
     public int getVertexGrade(String vertexLabel) {
         return this.getAdjacencyList(vertexLabel).size();
+    }
+
+    private Graph<T> deepFirstRecursiveTraverse() {
+        Graph<T> dfRTree =  new Graph<>();
+        String startKey = new Random().nextInt(this.adjacencyMap.size()) + "";
+
+        return this.deepFirstRecursiveTraverseAux(dfRTree, null, this.getVertex(startKey));
+    }
+
+    private Graph<T> deepFirstRecursiveTraverseAux(Graph<T> gTree, Vertex<T> parent, Vertex<T> current) {
+        if (!gTree.vertexExists(current)) {
+            gTree.addVertex(current);
+
+            if (parent != null) {
+                gTree.addDirectedEdge(parent.getLabel(), current.getLabel());
+            }
+
+            ArrayList<Vertex<T>> list = this.getAdjacencyList(current.getLabel());
+            for (Vertex<T> v : list) {
+                if (!gTree.vertexExists(v)) {
+                    gTree = this.deepFirstRecursiveTraverseAux(gTree, current, v);
+                }
+            }
+        }
+
+        return gTree;
+    }
+
+    private Graph<T> deepFirstTraverse() {
+        Graph<T> dfsTree =  new Graph<>();
+        String startKey = new Random().nextInt(this.adjacencyMap.size()) + "";
+
+        Stack<Vertex<T>> pendingVertexes =  new Stack<>();
+        Stack<Vertex<T>> currentParent =  new Stack<>();
+
+        pendingVertexes.push(this.getVertex(startKey));
+
+        while (!pendingVertexes.empty()) {
+            Vertex<T> current = pendingVertexes.peek();
+            if (!dfsTree.vertexExists(current)) {
+                dfsTree.addVertex(current);
+                if (!currentParent.empty()) {
+                    dfsTree.addDirectedEdge(currentParent.peek().getLabel(), current.getLabel());
+                }
+
+                ArrayList<Vertex<T>> list = this.getAdjacencyList(current.getLabel());
+                for (Vertex<T> v : list) {
+                    if (!dfsTree.vertexExists(v)) {
+                        pendingVertexes.push(v);
+                    }
+                }
+
+                currentParent.push(current);
+            } else {
+                current = pendingVertexes.pop();
+                if (!currentParent.empty() && current.equals(currentParent.peek())) {
+                    currentParent.pop();
+                }
+            }
+        }
+
+        return dfsTree;
+    }
+
+    private Graph<T> breadthFirstTraverse() {
+        Graph<T> bfsTree =  new Graph<>();
+        String startKey = new Random().nextInt(this.adjacencyMap.size()) + "";
+
+        Deque<Vertex<T>> queue =  new LinkedList<>();
+        queue.add(this.getVertex(startKey));
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.remove();
+            bfsTree.addVertex(current);
+
+            ArrayList<Vertex<T>> list = this.getAdjacencyList(current.getLabel());
+            for (Vertex<T> v : list) {
+                if (!bfsTree.vertexExists(v)) {
+                    queue.add(v);
+                    bfsTree.addVertex(v);
+                    bfsTree.addDirectedEdge(current.getLabel(), v.getLabel());
+                }
+            }
+        }
+
+        return bfsTree;
     }
 
     @Override
--- src/com/jroberto/cic/Main.java
+++ src/com/jroberto/cic/Main.java
@@ -1,22 +1,69 @@
 package com.jroberto.cic;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        // TESTS
-        GraphGenerator.genGilbert(30, 0.15, true, false).toCsv("Gilbert30");
-        GraphGenerator.genGilbert(100, 0.01, true, false).toCsv("Gilbert100");
-        GraphGenerator.genGilbert(500, 0.01, true, false).toCsv("Gilbert500");
-        GraphGenerator.genErdosRenyi(30, 90, true, false).toCsv("ErdosRenyi30");
-        GraphGenerator.genErdosRenyi(100, 300, true, false).toCsv("ErdosRenyi100");
-        GraphGenerator.genErdosRenyi(500, 1500, true, false).toCsv("ErdosRenyi500");
-        GraphGenerator.genGeographic(30, 0.15, true, false).toCsv("Geographic30");
-        GraphGenerator.genGeographic(100, 0.15, true, false).toCsv("Geographic100");
-        GraphGenerator.genGeographic(500, 0.15, true, false).toCsv("Geographic500");
-        GraphGenerator.genBarabasiAlbert(30, 2, true, false).toCsv("BarabasiAlbert30");
-        GraphGenerator.genBarabasiAlbert(100, 3, true, false).toCsv("BarabasiAlbert100");
-        GraphGenerator.genBarabasiAlbert(500, 6, true, false).toCsv("BarabasiAlbert500");
+        // 2nd Project Tests
+        GraphGenerator.genGilbert(50, 0.10, true, false).getDfTree().toCsv("GilbertDF30");
+        GraphGenerator.genGilbert(50, 0.10, true, false).getDfrTree().toCsv("GilbertDFR30");
+        GraphGenerator.genGilbert(50, 0.10, true, false).getBfTree().toCsv("GilbertBF30");
+
+        GraphGenerator.genGilbert(100, 0.01, true, false).getDfTree().toCsv("GilbertDF100");
+        GraphGenerator.genGilbert(100, 0.01, true, false).getDfrTree().toCsv("GilbertDFR100");
+        GraphGenerator.genGilbert(100, 0.01, true, false).getBfTree().toCsv("GilbertBF100");
+
+        GraphGenerator.genGilbert(500, 0.001, true, false).getDfTree().toCsv("GilbertDF500");
+        GraphGenerator.genGilbert(500, 0.001, true, false).getDfrTree().toCsv("GilbertDFR500");
+        GraphGenerator.genGilbert(500, 0.001, true, false).getBfTree().toCsv("GilbertBF500");
+
+        GraphGenerator.genErdosRenyi(50, 150, true, false).getDfTree().toCsv("ErdosRenyiDF30");
+        GraphGenerator.genErdosRenyi(50, 150, true, false).getDfrTree().toCsv("ErdosRenyiDFR30");
+        GraphGenerator.genErdosRenyi(50, 150, true, false).getBfTree().toCsv("ErdosRenyiBF30");
+
+        GraphGenerator.genErdosRenyi(100, 300, true, false).getDfTree().toCsv("ErdosRenyiDF100");
+        GraphGenerator.genErdosRenyi(100, 300, true, false).getDfrTree().toCsv("ErdosRenyiDFR100");
+        GraphGenerator.genErdosRenyi(100, 300, true, false).getBfTree().toCsv("ErdosRenyiBF100");
+
+        GraphGenerator.genErdosRenyi(500, 1500, true, false).getDfTree().toCsv("ErdosRenyiDF500");
+        GraphGenerator.genErdosRenyi(500, 1500, true, false).getDfrTree().toCsv("ErdosRenyiDFR500");
+        GraphGenerator.genErdosRenyi(500, 1500, true, false).getBfTree().toCsv("ErdosRenyiBF500");
+
+        GraphGenerator.genGeographic(50, 0.20, true, false).getDfTree().toCsv("GeographicDF30");
+        GraphGenerator.genGeographic(50, 0.20, true, false).getDfrTree().toCsv("GeographicDFR30");
+        GraphGenerator.genGeographic(50, 0.20, true, false).getBfTree().toCsv("GeographicBF30");
+
+        GraphGenerator.genGeographic(100, 0.20, true, false).getDfTree().toCsv("GeographicDF100");
+        GraphGenerator.genGeographic(100, 0.20, true, false).getDfrTree().toCsv("GeographicDFR100");
+        GraphGenerator.genGeographic(100, 0.20, true, false).getBfTree().toCsv("GeographicBF100");
+
+        GraphGenerator.genGeographic(500, 0.20, true, false).getDfTree().toCsv("GeographicDF500");
+        GraphGenerator.genGeographic(500, 0.20, true, false).getDfrTree().toCsv("GeographicDFR500");
+        GraphGenerator.genGeographic(500, 0.20, true, false).getBfTree().toCsv("GeographicBF500");
+
+        GraphGenerator.genBarabasiAlbert(50, 2, true, false).getDfTree().toCsv("BarabasiAlbertDF30");
+        GraphGenerator.genBarabasiAlbert(50, 2, true, false).getDfrTree().toCsv("BarabasiAlbertDFR30");
+        GraphGenerator.genBarabasiAlbert(50, 2, true, false).getBfTree().toCsv("BarabasiAlbertBF30");
+
+        GraphGenerator.genBarabasiAlbert(100, 3, true, false).getDfTree().toCsv("BarabasiAlbertDF100");
+        GraphGenerator.genBarabasiAlbert(100, 3, true, false).getDfrTree().toCsv("BarabasiAlbertDFR100");
+        GraphGenerator.genBarabasiAlbert(100, 3, true, false).getBfTree().toCsv("BarabasiAlbertBF100");
+
+        GraphGenerator.genBarabasiAlbert(500, 6, true, false).getDfTree().toCsv("BarabasiAlbertDF500");
+        GraphGenerator.genBarabasiAlbert(500, 6, true, false).getDfrTree().toCsv("BarabasiAlbertDFR500");
+        GraphGenerator.genBarabasiAlbert(500, 6, true, false).getBfTree().toCsv("BarabasiAlbertBF500");
+
+        // 1rst Project Tests
+//        GraphGenerator.genGilbert(50, 0.10, true, false).toCsv("Gilbert30");
+//        GraphGenerator.genGilbert(100, 0.01, true, false).toCsv("Gilbert100");
+//        GraphGenerator.genGilbert(500, 0.001, true, false).toCsv("Gilbert500");
+//        GraphGenerator.genErdosRenyi(50, 150, true, false).toCsv("ErdosRenyi30");
+//        GraphGenerator.genErdosRenyi(100, 300, true, false).toCsv("ErdosRenyi100");
+//        GraphGenerator.genErdosRenyi(500, 1500, true, false).toCsv("ErdosRenyi500");
+//        GraphGenerator.genGeographic(50, 0.20, true, false).toCsv("Geographic30");
+//        GraphGenerator.genGeographic(100, 0.20, true, false).toCsv("Geographic100");
+//        GraphGenerator.genGeographic(500, 0.20, true, false).toCsv("Geographic500");
+//        GraphGenerator.genBarabasiAlbert(50, 2, true, false).toCsv("BarabasiAlbert30");
+//        GraphGenerator.genBarabasiAlbert(100, 3, true, false).toCsv("BarabasiAlbert100");
+//        GraphGenerator.genBarabasiAlbert(500, 6, true, false).toCsv("BarabasiAlbert500");
     }
 }
